7a0afd523e2bf821ad541d57128cc9dcc024f159
uohzey <420709076@qq.com>
2022-05-19 11:33:32 +0800

    2022/5/19_morning

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -27,19 +27,58 @@
                 <a href="" class="btn">Contace Me <i class="fa fa-arrow-circle-right"></i></a>
             </div>
             <div class="home-img">
-                <img src="@/images/xiaoxue.jpg" alt="" style="height:30%;width:30%">
+                <img src="@/images/xiaoxue1.jpg" alt="">
             </div>
         </div>
-        <a href="#about" class="scrool-down">Scroll Down<i class="fa fa-arrow-down"></i></a>
+        <a href="#about" class="scroll-down">Scroll Down<i class="fa fa-arrow-down"></i></a>
     </section>
+    <section class="about section" id="about">
+        <div class="container flex-center">
+            <h1 class="section-title-01"></h1>
+            <h2 class="section-title-02"></h2>
+            <div class="content flex-center">
+                <div class="about-img">
+                    <img src="@/images/about-img.jpg" alt="">
+                </div>
+                <div class="about-info">
+                    <div class="description">
+                        <h3>I'm uohzey</h3>
+                        <h4>A student now studing at USTC</h4>
+                        <p></p>
+                    </div>
+                    <ul class="professional-list">
+                        <li class="list-item">
+                            <h3>5+</h3>
+                            <span>Years of<br>Experience</span>
+                        </li>
+                        <li class="list-item">
+                            <h3>3K+</h3>
+                            <span>Happy<br>Customers</span>
+                        </li>
+                        <li class="list-item">
+                            <h3>5+</h3>
+                            <span>Success<br>Projects</span>
+                        </li>
+                    </ul>
+                    <a href="" class="btn">Download CV <i class="fa fa-download"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <submission-chart :profile="profile"></submission-chart>
 </template>
 
 <script>
 import { defineComponent } from "@vue/runtime-core"
+import SubmissionChart from "./SubmissionChart.vue"
 export default defineComponent({
     name: 'MyHeader',
+    components: { SubmissionChart },
     data() {
         return {
+            profile: [{
+                '2022-01-01': 10
+            }],
             size: 2,
         }
     }
